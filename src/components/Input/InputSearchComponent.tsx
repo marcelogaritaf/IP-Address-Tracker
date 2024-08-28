d602026ffdc0fb10902000f611d1393766113631
Marcelo Garita <mgaritaf@outlook.com>
2024-08-28 15:47:18 -0600

    inputComponent styles

--- src/components/Input/InputSearchComponent.tsx
+++ src/components/Input/InputSearchComponent.tsx
@@ -1,5 +1,22 @@
+import { useRef } from "react";
+import "./inputCSS.css";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 const InputSearchComponent = () => {
-  return <div>InputSearchComponent</div>;
+  const inputRef = useRef(null);
+  return (
+    <div className="input-section">
+      <form>
+        <input
+          type="text"
+          placeholder="Search for any IP address or domain"
+          ref={inputRef}
+        />
+        <button>
+          <MdOutlineArrowForwardIos />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default InputSearchComponent;
--- src/components/title/IpTrackerTitle.tsx
+++ src/components/title/IpTrackerTitle.tsx
@@ -1,8 +1,10 @@
+import InputSearchComponent from "../Input/InputSearchComponent";
 import "./titleCSS.css";
 const IpTrackerTitle = () => {
   return (
     <div className="background-title">
       <h1>IP Address Tracker</h1>
+      <InputSearchComponent />
     </div>
   );
 };
