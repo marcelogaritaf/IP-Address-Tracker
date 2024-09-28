c492dfcab507fb9014102f3090aaa331cc322e21
Marcelo Garita <mgaritaf@outlook.com>
2024-09-27 18:35:44 -0600

    creating map-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import InfoIPTrackerComponent from "./components/info/InfoIPTrackerComponent";
+import MapComponent from "./components/Map/MapComponent";
 import IpTrackerTitle from "./components/title/IpTrackerTitle";
-
+import "./index.css";
 function App() {
   return (
-    <div className="hola">
-      <IpTrackerTitle />
-      <InfoIPTrackerComponent />
-    </div>
+    <>
+      <div className="main-container">
+        <IpTrackerTitle />
+        <InfoIPTrackerComponent />
+      </div>
+      <div className="map">
+        <MapComponent />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Map/MapComponent.tsx
@@ -0,0 +1,28 @@
+import { LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
+const MapComponent = () => {
+  const position: LatLngExpression = [51.505, -0.09];
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{
+        height: "100vh",
+        width: "100%",
+        position: "absolute",
+        zIndex: "2",
+      }}
+    >
+      <TileLayer
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>i'm in london</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
